f106166ac9589cb9b880b2226c731e554babbb9c
MCZADOWSKI <czadomax@gmail.com>
2021-06-08 10:40:40 +0200

    login qui casse tout ne casse plus tout :)

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/gorilla/sessions"
 	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -18,90 +19,90 @@ type Login struct {
 }
 
 type Err struct {
-	Nope string
+	Nope         string
+	WrongPass    string
+	UserNotFound string
 }
 
-type postInfo struct {
-	idx     int
-	pseudo  string
-	message string
-}
-type postMessage struct {
-	id      int
-	image   string
-	pseudo  string
-	titre   string
-	message string
-}
-
-type oui struct {
-	Poste   []postMessage
-	Comment []postMessage
+type Post struct {
+	Comment    string
+	Title      string
+	TagCod     string
+	TagMusic   string
+	TagArt     string
+	TagSport   string
+	TagFashion string
+	TagFood    string
+	TagCinema  string
+	TagCars    string
 }
 
+var errToSend Err
+var store = sessions.NewCookieStore([]byte("mysession"))
 var dataLogin Login
+var tag Post
+var database, _ = sql.Open("sqlite3", "data.db")
 
-func database(username string, email string, password string) {
+func checkErr(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
 
-	database, _ :=
-		sql.Open("sqlite3", "data.db")
+func createTablePeople() {
 	statement, _ :=
 		database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, username TEXT, email TEXT, password TEXT)")
+	defer statement.Close()
 	statement.Exec()
-	statement, _ =
+}
+
+func createTablePost() {
+	statement, _ :=
+		database.Prepare("CREATE TABLE IF NOT EXISTS post (id INTEGER PRIMARY KEY, username TEXT, title TEXT, comment TEXT)")
+	defer statement.Close()
+	statement.Exec()
+}
+
+func createTableComment() {
+	statement, _ :=
+		database.Prepare("CREATE TABLE IF NOT EXISTS comment (id INTEGER PRIMARY KEY, username TEXT, newComment TEXT)")
+	defer statement.Close()
+	statement.Exec()
+}
+
+func addUser(username string, email string, password string) {
+
+	statement, _ :=
 		database.Prepare("INSERT INTO people (username, email, password) VALUES (?, ?, ?)")
+	defer statement.Close()
 	statement.Exec(username, email, password)
 
-	rows, _ :=
-		database.Query("SELECT id, username, email, password FROM people")
+	lastRow := database.QueryRow("SELECT * FROM user WHERE id=(SELECT max(id) FROM user)")
+	var id int
+	lastRow.Scan(&id, &email, &username, &password)
+	fmt.Println("NEW USER : ", username)
+
+}
+
+func addPost(username string) {
+	fmt.Println(username)
+	fmt.Println(tag.Title)
+	fmt.Println(tag.Comment)
+	// fmt.Println(tag.TagCars)
+
+	statement, err :=
+		database.Prepare("INSERT INTO post (username, title, comment) VALUES (?, ?, ?)")
+	// defer statement.Close()
+	statement.Exec(username, tag.Title, tag.Comment)
+	checkErr(err)
+	rows, err :=
+		database.Query("SELECT id, username, title, comment FROM post")
+	checkErr(err)
 	var id int
 	fmt.Println("select data")
 	for rows.Next() {
-		rows.Scan(&id, &username, &email, &password)
-	}
-	database.Close()
-
-}
-
-func databasePost(username string, newPost string) {
-
-	database, _ :=
-		sql.Open("sqlite3", "data.db")
-	statement, _ :=
-		database.Prepare("CREATE TABLE IF NOT EXISTS post (id INTEGER PRIMARY KEY, username TEXT, newPost TEXT)")
-	statement.Exec()
-	statement, _ =
-		database.Prepare("INSERT INTO post (username, newPost) VALUES (?, ?)")
-	fmt.Println("ici")
-	statement.Exec(username, newPost)
-	rows, _ :=
-		database.Query("SELECT id, username, newPost FROM post")
-	var id int
-	var test []string
-	for rows.Next() {
-		rows.Scan(&id, &username, &newPost)
-		test = append(test, strconv.Itoa(id)+": "+username+" "+newPost+"\n")
-	}
-
-}
-
-func databaseComment(id int, username string, newComment string) {
-
-	database, _ :=
-		sql.Open("sqlite3", "data.db")
-	statement, _ :=
-		database.Prepare("CREATE TABLE IF NOT EXISTS comment (id INTEGER , username TEXT, newComment TEXT)")
-	statement.Exec()
-	statement, _ =
-		database.Prepare("INSERT INTO comment (id, username, newComment) VALUES (?, ?, ?)")
-	fmt.Println("ici")
-	statement.Exec(id, username, newComment)
-	rows, _ :=
-		database.Query("SELECT id, username, newComment FROM comment")
-	var test []string
-	for rows.Next() {
-		rows.Scan(&id, &username, &newComment)
-		test = append(test, strconv.Itoa(id)+": "+username+" "+newComment+"\n")
+		rows.Scan(&id, &username, &tag.Title, &tag.Comment)
+		fmt.Println(strconv.Itoa(id) + ": " + username + " " + tag.Title + " " + tag.Comment)
 	}
 
 }
@@ -121,9 +122,8 @@ func verifRegisterData(regUser string, regEmail string) bool {
 	var email string
 	var password string
 	var id int
+	var result = true
 
-	database, _ :=
-		sql.Open("sqlite3", "data.db")
 	rows, _ :=
 		database.Query("SELECT id, username, email, password FROM people")
 	for rows.Next() {
@@ -131,54 +131,72 @@ func verifRegisterData(regUser string, regEmail string) bool {
 		// fmt.Println(strconv.Itoa(id) + ": " + username + " " + email + " " + password)
 		if regEmail == email || regUser == username {
 			fmt.Println("nope")
-			database.Close()
-			return false
+			result = false
 		}
 	}
 	// fmt.Println("data : ", username, email)
 	// fmt.Println("paramètre :", regUser, regEmail)
-	database.Close()
-	return true
+	return result
 }
 
-func login(logUser string, logPassword string) bool {
+func login(logUser string, logPassword string) int {
 	var username string
 	var email string
 	var password string
 	var id int
+	var result = 0
 
-	database, _ :=
-		sql.Open("sqlite3", "data.db")
 	rows, _ :=
 		database.Query("SELECT id, username, email, password FROM people")
 	for rows.Next() {
 		rows.Scan(&id, &username, &email, &password)
 		// fmt.Println(strconv.Itoa(id) + ": " + username + " " + email + " " + password)
 		if logUser == username && CheckPasswordHash(logPassword, password) {
-			fmt.Println("correct password")
-			database.Close()
-			return true
+			result = 1
+		} else if logUser == username && !(CheckPasswordHash(logPassword, password)) {
+			result = 2
 		}
 	}
-	database.Close()
-	fmt.Println("wrong password")
-	return false
+	return result
+}
+
+func logoutHandle(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "mysession")
+	session.Options.MaxAge = -1
+	session.Save(r, w)
+	http.Redirect(w, r, "/main", http.StatusSeeOther)
 }
 
 func LoginHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("home")
-
 	dataLogin.Username = r.FormValue("username")
 	dataLogin.Password = r.FormValue("password")
+	errToSend.WrongPass = ""
+	errToSend.UserNotFound = ""
+	tpl := template.Must(template.ParseFiles("assets/index.html"))
+
 	if dataLogin.Password != "" && dataLogin.Username != "" {
-		if login(dataLogin.Username, dataLogin.Password) {
+		if login(dataLogin.Username, dataLogin.Password) == 1 {
+			fmt.Println("correct password")
+			session, _ := store.Get(r, "mysession")
+			session.Values["username"] = dataLogin.Username
+			session.Save(r, w)
 			http.Redirect(w, r, "/account", http.StatusSeeOther)
+		} else if login(dataLogin.Username, dataLogin.Password) == 2 {
+			fmt.Println("Wrong password")
+			errToSend.WrongPass = "Wrong password"
+			tpl.Execute(w, errToSend)
+			return
+		} else {
+			fmt.Println("user doesn't exist")
+			errToSend.UserNotFound = "user doesn't exist"
+			tpl.Execute(w, errToSend)
+			return
 		}
 
 	}
-	tpl := template.Must(template.ParseFiles("assets/index.html"))
 
-	err := tpl.Execute(w, dataLogin)
+	err := tpl.Execute(w, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -198,9 +216,7 @@ func RegisterHandle(w http.ResponseWriter, r *http.Request) {
 
 	if userName != "" && email != "" && password != "" {
 		if verifRegisterData(userName, email) {
-			database(userName, email, password)
-
-			fmt.Println("retourvers la page main")
+			addUser(userName, email, password)
 			http.Redirect(w, r, "/main", http.StatusSeeOther)
 		} else {
 			regData.Nope = "your email or your username are already used ! chacal"
@@ -219,20 +235,14 @@ func RegisterHandle(w http.ResponseWriter, r *http.Request) {
 func PostHandle(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Connect")
 
-	userName := r.FormValue("username")
-	newPost := r.FormValue("newPost")
-
-	if userName != "" && newPost != "" {
-		databasePost(userName, newPost)
-		fmt.Println("tu sors de  wsh")
+	session, _ := store.Get(r, "mysession")
+	userName := fmt.Sprintf("%v", session.Values["username"])
+	data := map[string]interface{}{
+		"username": userName,
 	}
 
 	tpl := template.Must(template.ParseFiles("assets/signIn.html"))
 
-	data := oui{
-		Poste: getPostInfo(),
-	}
-
 	err := tpl.Execute(w, data)
 	if err != nil {
 		log.Fatal(err)
@@ -240,98 +250,43 @@ func PostHandle(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func CommentHandle(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Connect")
+func CreatePostHandle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("POST")
 
-	id := r.FormValue("id")
-	userName := r.FormValue("username")
-	newComment := r.FormValue("newComment")
-	intId, _ := strconv.Atoi(id)
-	if userName != "" && newComment != "" {
-		databaseComment(intId, userName, newComment)
-		fmt.Println("tu sors de  wsh")
+	session, _ := store.Get(r, "mysession")
+	username := fmt.Sprintf("%v", session.Values["username"])
+
+	tag.Title = r.FormValue("to_post")
+	tag.Comment = r.FormValue("comment")
+	tag.TagCod = r.FormValue("tagCod")
+	tag.TagMusic = r.FormValue("tagMusic")
+	tag.TagArt = r.FormValue("tagArt")
+	tag.TagSport = r.FormValue("tagSport")
+	tag.TagFashion = r.FormValue("tagFashion")
+	tag.TagFood = r.FormValue("tagFood")
+	tag.TagCinema = r.FormValue("tagCinema")
+	tag.TagCars = r.FormValue("tagCars")
+
+	if tag.Comment != "" && tag.Title != "" {
+		fmt.Printf("psot en cour\n")
+		addPost(username)
 	}
 
-	tpl := template.Must(template.ParseFiles("assets/comment.html"))
-
-	data := oui{
-		Poste:   getPostInfo(),
-		Comment: getCommentInfo(),
-	}
-
-	err := tpl.Execute(w, data)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	tpl := template.Must(template.ParseFiles("assets/post.html"))
+	tpl.Execute(w, nil)
 }
-
-func getPostInfo() []postMessage {
-
-	database, _ :=
-		sql.Open("sqlite3", "data.db")
-	rows, _ :=
-		database.Query("SELECT id, username, newPost FROM post")
-
-	var _id int
-	var test []postMessage
-	var _pseudo string
-	var _message string
-	for rows.Next() {
-		rows.Scan(&_id, &_pseudo, &_message)
-		data := postMessage{
-			id:      _id,
-			pseudo:  _pseudo,
-			message: _message,
-		}
-		test = append(test, data)
-	}
-
-	return test
-
-}
-
-func getCommentInfo() []postMessage {
-
-	database, _ :=
-		sql.Open("sqlite3", "data.db")
-	rows, _ :=
-		database.Query("SELECT id, username, newComment FROM comment")
-
-	var _id int
-	var test []postMessage
-	var _pseudo string
-	var _message string
-	for rows.Next() {
-		rows.Scan(&_id, &_pseudo, &_message)
-		data := postMessage{
-			id:      _id,
-			pseudo:  _pseudo,
-			message: _message,
-		}
-		test = append(test, data)
-		fmt.Println(test)
-	}
-	return test
-}
-
-// func account(w http.ResponseWriter, r *http.Request) {
-
-// 	tpl := template.Must(template.ParseFiles("assets/signIn.html"))
-
-// 	err := tpl.Execute(w, dataLogin)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
 
 func main() {
+	createTablePeople()
+	createTablePost()
+	createTableComment()
 	fs := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.Handle("/", fs)
 	http.HandleFunc("/main", LoginHandle)
+	http.HandleFunc("/logout", logoutHandle)
 	http.HandleFunc("/connect", RegisterHandle)
 	http.HandleFunc("/account", PostHandle)
-	http.HandleFunc("/comment", CommentHandle)
+	http.HandleFunc("/CreateNewPost", CreatePostHandle)
 	http.ListenAndServe(":8080", nil)
 }
